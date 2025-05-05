fbb56b1cead0ee5660e88ac81a796d62e74b6bd6
Dammouz <dammouz.dev@gmail.com>
2025-05-04 22:10:56 -0400

    Add base 64 encoding
    Add base 64 encoding and decoding,
    and associated tests.

---
+++ src/Common.Tests/Text/Base64Tests.cs
@@ -0,0 +1,25 @@
+using Common.Text;
+using NUnit.Framework;
+
+namespace Common.Tests.Text
+{
+    [TestFixture]
+    public sealed class Base64Tests
+    {
+
+        [Test]
+        public void Convertb64()
+        {
+            // Arrange
+            const string CodeToEncode = "MyCodeToto123";
+
+            // Act
+            var base64Encoded = Base64.Base64Encode(CodeToEncode);
+            var base64Decoded = Base64.Base64Decode(base64Encoded);
+
+            // Assert
+            Assert.That(base64Decoded, Is.EqualTo(CodeToEncode),
+               $"We should have decoded base 64 with is equal to the initial {nameof(CodeToEncode)}");
+        }
+    }
+}
